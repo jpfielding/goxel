591960282d17ba9ebc09931703597f884ef1249e
JP Fielding <jp.fielding@gmail.com>
2025-09-17 17:02:36 +0000

    sig handlers and logging

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"log"
+	"log/slog"
 	"os"
+	"os/signal"
 	"strconv"
+	"syscall"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
@@ -16,6 +20,7 @@ import (
 	"fyne.io/fyne/v2/storage"
 	"fyne.io/fyne/v2/widget"
 
+	"github.com/jpfielding/goxel/pkg/logging"
 	"github.com/suyashkumar/dicom"
 	"github.com/suyashkumar/dicom/pkg/frame"
 	"github.com/suyashkumar/dicom/pkg/tag"
@@ -164,7 +169,23 @@ func fileLength(path string) int64 {
 	return info.Size()
 }
 
+var (
+	GitSHA string = "NA"
+)
+
 func main() {
+	// register sigterm for graceful shutdown
+	ctx, cnc := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer cnc()
+	go func() {
+		defer cnc() // this cnc is from notify and removes the signal so subsequent ctrl-c will restore kill functions
+		<-ctx.Done()
+	}()
+	slog.SetDefault(logging.Logger(os.Stdout, false, slog.LevelInfo))
+	ctx = logging.AppendCtx(ctx,
+		slog.Group("goxel",
+			slog.String("git", GitSHA),
+		))
 	a := app.New()
 	a.SetIcon(resourceIconPng)
 
