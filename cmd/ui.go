ab2af98f92fa7e9fe02e772d49b6e31f9b67a204
JP Fielding <jp.fielding@gmail.com>
2025-10-06 20:29:39 -0400

    move ui to ui.go

--- cmd/ui.go
+++ cmd/ui.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log/slog"
 	"os"
 	"strconv"
 
@@ -13,6 +15,10 @@ import (
 	"fyne.io/fyne/v2/storage"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
+	"github.com/jpfielding/goxel/pkg/dicos"
+	"github.com/suyashkumar/dicom"
+	"github.com/suyashkumar/dicom/pkg/frame"
+	"github.com/suyashkumar/dicom/pkg/tag"
 )
 
 var (
@@ -32,6 +38,170 @@ var (
 		"Mediastinum": {50, 350},
 	}
 )
+
+type viewer struct {
+	dicom                  *DICOMImage
+	frames                 []*frame.Frame
+	currentFrame           int
+	image                  *canvas.Image
+	study, name, id, frame *widget.Label
+	level, width           *widget.Entry
+
+	win fyne.Window
+}
+
+func (v *viewer) loadDir(dir fyne.ListableURI) {
+	var (
+		data   dicom.Dataset
+		frames []*frame.Frame
+	)
+
+	files, _ := dir.List()
+	for i, file := range files {
+		r, _ := storage.Reader(file)
+		d, err := dicom.Parse(r, fileLength(file.Path()), nil)
+		if i == 0 {
+			if err != nil {
+				slog.Error("first file in dir was not DICOM", slog.Any("error", err))
+				return
+			}
+			data = d
+		}
+		if err != nil {
+			slog.Error("could not open dicom file "+file.Name()+" in folder", slog.Any("error", err))
+			continue
+		}
+
+		t, err := d.FindElementByTag(tag.PixelData)
+		if err == nil {
+			frames = append(frames, t.Value.GetValue().(dicom.PixelDataInfo).Frames...)
+		}
+		_ = r.Close()
+	}
+
+	t, err := data.FindElementByTag(tag.PixelData)
+	if err == nil {
+		info := t.Value.GetValue().(dicom.PixelDataInfo)
+		info.Frames = frames
+		v, _ := dicom.NewValue(info)
+		t.Value = v
+	}
+
+	v.loadImage(data)
+}
+
+func (v *viewer) loadFile(r io.ReadCloser, length int64) {
+	data, err := dicom.Parse(r, length, nil)
+	if err != nil {
+		dialog.ShowError(err, v.win)
+		return
+	}
+	defer r.Close()
+	v.loadImage(data)
+}
+
+func (v *viewer) loadImage(data dicom.Dataset) {
+	for _, elem := range data.Elements {
+		switch elem.Tag {
+		case tag.PixelData:
+			v.frames = elem.Value.GetValue().(dicom.PixelDataInfo).Frames
+
+			if len(v.frames) == 0 {
+				panic("No images found")
+			}
+			v.setFrame(0)
+		case tag.PatientName:
+			v.name.SetText(fmt.Sprintf("%v", elem.Value))
+		case tag.PatientID:
+			v.id.SetText(fmt.Sprintf("%v", elem.Value))
+		case tag.StudyDescription:
+			v.study.SetText(fmt.Sprintf("%v", elem.Value))
+		case tag.WindowCenter:
+			str := fmt.Sprintf("%v", elem.Value.GetValue().([]string)[0])
+			l, _ := strconv.Atoi(str)
+			v.dicom.SetWindowLevel(int16(l))
+			v.level.SetText(str)
+		case tag.WindowWidth:
+			str := fmt.Sprintf("%v", elem.Value.GetValue().([]string)[0])
+			l, _ := strconv.Atoi(str)
+			v.dicom.SetWindowWidth(int16(l))
+			v.width.SetText(str)
+		default:
+			t := elem.Tag
+			key := t.String()
+			if ltn := dicos.LookupTagName(t.Group, t.Element); ltn != "" {
+				key = ltn
+			}
+			switch elem.Value.ValueType() {
+			case dicom.Strings:
+			// Bytes represents an underlying value of []byte
+			case dicom.Bytes:
+			// Ints represents an underlying value of []int
+			case dicom.Ints:
+				// PixelData represents an underlying value of PixelDataInfo
+			case dicom.PixelData:
+				// SequenceItem represents an underlying value of []*Element
+			case dicom.SequenceItem:
+				// Sequences represents an underlying value of []SequenceItem
+			case dicom.Sequences:
+				// Floats represents an underlying value of []float64
+			case dicom.Floats:
+			default:
+				slog.Info("tag", slog.Any(key, elem.Value.String()))
+			}
+		}
+	}
+}
+
+func (v *viewer) loadKeys() {
+	v.win.Canvas().SetOnTypedKey(func(key *fyne.KeyEvent) {
+		switch key.Name {
+		case fyne.KeyUp:
+			v.nextFrame()
+		case fyne.KeyDown:
+			v.previousFrame()
+		case fyne.KeyF:
+			v.fullScreen()
+		}
+	})
+}
+
+func (v *viewer) nextFrame() {
+	v.setFrame(v.currentFrame + 1)
+}
+
+func (v *viewer) previousFrame() {
+	v.setFrame(v.currentFrame - 1)
+}
+
+func (v *viewer) setFrame(id int) {
+	count := len(v.frames)
+	if id > count-1 {
+		id = 0
+	} else if id < 0 {
+		id = count - 1
+	}
+	v.currentFrame = id
+
+	v.dicom.SetFrame(&v.frames[id].NativeData)
+	canvas.Refresh(v.image)
+	v.frame.SetText(fmt.Sprintf("%d/%d", id+1, count))
+}
+
+func fileLength(path string) int64 {
+	f, err := os.Open(path)
+	if err != nil {
+		return 0
+	}
+	defer f.Close()
+
+	info, err := f.Stat()
+	if err != nil {
+		return 0
+	}
+
+	return info.Size()
+}
 
 func (v *viewer) fullScreen() {
 	v.win.SetFullScreen(!v.win.FullScreen())
